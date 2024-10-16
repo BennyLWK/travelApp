eddfcfde839c91b254459d4bb8b1c168a97ff5f4
bennylwk <bennyliew@live.co.uk>
2024-10-16 10:24:11 +0800

    【REMOVED】unused image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,5 @@
-export const skiVilla = require('../assets/images/ski_villa.jpg');
 export const skiVillaBanner = require('../assets/images/ski_villa_banner.jpg');
 
 export default {
-  skiVilla,
   skiVillaBanner,
 };
